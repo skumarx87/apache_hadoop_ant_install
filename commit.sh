5e060ca72686c7fc62367a1058c77b1fd2679c2b
sathish <sathish@laksha.home.com>
2021-11-30 13:44:14 +0530

    initial commit

---
+++ commit.sh
@@ -0,0 +1,11 @@
+git add hadoop_install.xml 
+git add bigdata.properties 
+git add scripts/*
+git add conf/*
+git add commit.sh
+
+git commit -am "initial commit"
+
+git push origin
+#git push origin HEAD:master
+
